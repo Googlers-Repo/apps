e5feabecba17a28a2db9bd1d1e1b3b546f70adc1
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2022-07-20 13:48:19 +0200

    .

---
+++ generate.py
@@ -0,0 +1,78 @@
+import sys
+import json
+import os
+from github import Github
+
+# Configuration
+REPO_NAME = "Googlers-Magisk-Repo"
+REPO_TITLE = "Googlers Store Repo"
+REPO_WEBSITE = "https://dergoogler.com/repo"
+REPO_SUPPORT = "https://t.me/The_Googler"
+REPO_DONATE = None
+
+# Skeleton for the repository
+meta = {
+    "last_update": "",
+    "name": REPO_TITLE,
+    "website": REPO_WEBSITE,
+    "support": REPO_SUPPORT,
+    "donate": REPO_DONATE,
+    "apps": []
+}
+
+# Initialize the GitHub objects
+g = Github(os.environ['GIT_TOKEN'])
+user = g.get_user(REPO_NAME)
+repo = user.get_repo("store-index")
+
+# Fetch the last repository update
+meta["last_update"] = int(user.updated_at.timestamp() * 1000)
+
+
+contents = repo.get_contents("")
+while len(contents) > 0:
+    try:
+        file_content = contents.pop(0)
+
+        if file_content.type == 'dir':
+            # Ignore these folders
+            # if file_content.path != ".github" & file_content.path != "core":
+            # Parse module.prop into a python object
+            moduleprop_raw = repo.get_contents(
+                f"{file_content.path}/app.prop").decoded_content.decode("UTF-8")
+
+            moduleprop = {}
+            for line in moduleprop_raw.splitlines():
+                if "=" not in line:
+                    continue
+                lhs, rhs = line.split("=", 1)
+                moduleprop[lhs] = rhs
+
+            def getProp(name, replace="", replaceValue=""):
+                try:
+                    return moduleprop[name].replace(replace, replaceValue)
+                except:
+                    return None
+
+            details = {
+                "name": getProp("name"),
+                "icon": getProp("icon", "{localURL}", f"https://raw.githubusercontent.com/{repo.full_name}/{repo.default_branch}/{file_content.path}"),
+                "package": getProp("package"),
+                "versionName": getProp("versionName"),
+                "versionCode": getProp("versionCode"),
+            }
+
+            apps = {
+                "apk_url":  getProp("download"),
+                "readme_url": f"https://raw.githubusercontent.com/{repo.full_name}/{repo.default_branch}/{file_content.path}/README.md",
+                "prop": details,
+            }
+
+            # Append to skeleton
+            meta["apps"].append(apps)
+    except Exception as e:
+        # print(e)
+        continue
+
+# Return our final skeleton
+print(json.dumps(meta, indent=4))
